72d580fcf6befdafd9565230dd97d4ea29a5f2ba
cybeloras <ascott18@gmail.com>
2011-10-09 23:22:42 +0000

    ** (r253) TellMeWhen_Options\TellMeWhen_Options.lua:2246: attempt to index global "current" (a nil value)

--- TellMeWhen_Options/TellMeWhen_Options.lua
+++ TellMeWhen_Options/TellMeWhen_Options.lua
@@ -2243,8 +2243,8 @@ end
 
 function IE:ShowHelp(text, frame, x, y, noclose, ...)
 	text = format(text, ...)
-	if current.noclose then return end
 	local current = IE.Help.current
+	if current.noclose then return end
 		current.text = text
 		current.frame = frame
 		current.x = x
