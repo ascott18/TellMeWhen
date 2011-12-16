be1ffa10a2f3022e063e4cab101667097a5279cc
cybeloras <ascott18@gmail.com>
2011-12-16 22:47:06 +0000

    ** (r345) TellMeWhen.lua:1467 table index is nil

--- TellMeWhen.lua
+++ TellMeWhen.lua
@@ -32,7 +32,7 @@ local DRData = LibStub("DRData-1.0", true)
 TELLMEWHEN_VERSION = "4.7.2"
 TELLMEWHEN_VERSION_MINOR = strmatch(" @project-version@", " r%d+") or ""
 TELLMEWHEN_VERSION_FULL = TELLMEWHEN_VERSION .. TELLMEWHEN_VERSION_MINOR
-TELLMEWHEN_VERSIONNUMBER = 47201 -- NEVER DECREASE THIS NUMBER (duh?).  IT IS ALSO ONLY INTERNAL
+TELLMEWHEN_VERSIONNUMBER = 47202 -- NEVER DECREASE THIS NUMBER (duh?).  IT IS ALSO ONLY INTERNAL
 if TELLMEWHEN_VERSIONNUMBER > 48000 or TELLMEWHEN_VERSIONNUMBER < 47000 then return error("YOU SCREWED UP THE VERSION NUMBER OR DIDNT CHANGE THE SAFETY LIMITS") end -- safety check because i accidentally made the version number 414069 once
 
 TELLMEWHEN_MAXGROUPS = 1 	--this is a default, used by SetTheory (addon), so dont rename
@@ -1457,14 +1457,13 @@ function TMW:OnUpdate()					-- this is where all icon OnUpdate scripts are actua
 	end
 end
 
-
 function TMW:Update()
 	if not (TMW.EnteredWorld and TMW.VarsLoaded) then return end
 
 	if not TMW.Warned then
 		for k, v in ipairs(TMW.Warn) do
 			TMW:Print(v)
-			TMW.Warn[text] = true
+			TMW.Warn[k] = true
 		end
 		TMW.Warned = true
 	end
