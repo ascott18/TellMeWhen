dd658b99b43366a7124e6e68d1926f06b17c5832
cybeloras <ascott18@gmail.com>
2012-11-10 01:06:13 +0000

    Fixed syntax errors

--- TellMeWhen.lua
+++ TellMeWhen.lua
@@ -30,7 +30,7 @@ local DogTag = LibStub("LibDogTag-3.0", true)
 TELLMEWHEN_VERSION = "6.0.4"
 TELLMEWHEN_VERSION_MINOR = strmatch(" @project-version@", " r%d+") or ""
 TELLMEWHEN_VERSION_FULL = TELLMEWHEN_VERSION .. TELLMEWHEN_VERSION_MINOR
-TELLMEWHEN_VERSIONNUMBER = 60446 -- NEVER DECREASE THIS NUMBER (duh?).  IT IS ALSO ONLY INTERNAL
+TELLMEWHEN_VERSIONNUMBER = 60447 -- NEVER DECREASE THIS NUMBER (duh?).  IT IS ALSO ONLY INTERNAL
 if TELLMEWHEN_VERSIONNUMBER > 61001 or TELLMEWHEN_VERSIONNUMBER < 60000 then return error("YOU SCREWED UP THE VERSION NUMBER OR DIDNT CHANGE THE SAFETY LIMITS") end -- safety check because i accidentally made the version number 414069 once
 
 TELLMEWHEN_MAXROWS = 20
@@ -2370,7 +2370,7 @@ function TMW:GetBaseUpgrades()			-- upgrade functions
 				{class = "PALADIN", 	id = 7294}, 	-- Retribution Aura
 
 				{class = "WARLOCK", 	id = 47241}, 	-- Metamorphosis
-			}
+			},
 	
 			setupcsn = function(self)
 				self.CSN = {
@@ -2557,7 +2557,7 @@ function TMW:GetBaseUpgrades()			-- upgrade functions
 				{class = "PALADIN", 	id = 7294}, 	-- Retribution Aura
 
 				{class = "WARLOCK", 	id = 47241}, 	-- Metamorphosis
-			}
+			},
 	
 			setupcsn = function(self)
 				self.CSN = {
