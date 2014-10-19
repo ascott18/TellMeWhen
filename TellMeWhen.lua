13f9d51cd34a43fc0d85171383b4ea17341251e9
cybeloras <ascott18@gmail.com>
2014-10-18 17:26:48 -0700

    TellMeWhen will no longer perform a full setup in response to PLAYER_SPECIALIZATION_CHANGED or PLAYER_TALENT_UPDATE while in combat, because it seems that in WoD, these events will sometimes fire while in combat.

--- TellMeWhen.lua
+++ TellMeWhen.lua
@@ -26,7 +26,7 @@ elseif strmatch(projectVersion, "%-%d+%-") then
 end
 
 TELLMEWHEN_VERSION_FULL = TELLMEWHEN_VERSION .. " " .. TELLMEWHEN_VERSION_MINOR
-TELLMEWHEN_VERSIONNUMBER = 72001 -- NEVER DECREASE THIS NUMBER (duh?).  IT IS ALSO ONLY INTERNAL (for versioning of)
+TELLMEWHEN_VERSIONNUMBER = 72002 -- NEVER DECREASE THIS NUMBER (duh?).  IT IS ALSO ONLY INTERNAL (for versioning of)
 
 TELLMEWHEN_FORCECHANGELOG = 71030 -- if the user hasn't seen the changelog until at least this version, show it to them.
 
@@ -3399,10 +3399,12 @@ function TMW:UpdateTalentTextureCache()
 end
 
 function TMW:PLAYER_SPECIALIZATION_CHANGED(...)
-	TMW:ScheduleUpdate(.2)
-	--TMW:Update()
+	if not InCombatLockdown() then
+		TMW:ScheduleUpdate(.2)
+		--TMW:Update()
 
-	TMW:UpdateTalentTextureCache()
+		TMW:UpdateTalentTextureCache()
+	end
 end
 
 function TMW:OnProfile(event, arg2, arg3)
