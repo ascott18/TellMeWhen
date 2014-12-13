fc8fb0c8b2216995f7d2523b1082c0676f3fcfb2
cybeloras <ascott18@gmail.com>
2014-12-13 11:42:43 -0800

    Fixed some issues with the item w/ slotIDs suggestion module

--- Components/IconTypes/IconType_item/Config.lua
+++ Components/IconTypes/IconType_item/Config.lua
@@ -56,6 +56,35 @@ end
 local Module = SUG:NewModule("itemwithslots", SUG:GetModule("item"))
 Module.Slots = {}
 
+local slotNames = {}
+for _, slotName in pairs{
+	"BackSlot",
+	"ChestSlot",
+	"FeetSlot",
+	"Finger0Slot",
+	"Finger1Slot",
+	"HandsSlot",
+	"HeadSlot",
+	"LegsSlot",
+	"MainHandSlot",
+	"NeckSlot",
+	"SecondaryHandSlot",
+	"ShirtSlot",
+	"ShoulderSlot",
+	"TabardSlot",
+	"Trinket0Slot",
+	"Trinket1Slot",
+	"WaistSlot",
+	"WristSlot",
+} do
+	local slotID = GetInventorySlotInfo(slotName)
+	if slotID then
+		slotNames[slotID] = slotName
+	else
+		TMW:Debug("Invalid slot name %s", slotName)
+	end
+end
+
 function Module:OnSuggest()
 	ItemCache_Cache = ItemCache:GetCache()
 end
@@ -66,16 +95,23 @@ function Module:Entry_AddToList_2(f, id)
 
 		f.overrideInsertID = L["SUG_INSERTITEMSLOT"]
 
-		local name = GetItemInfo(itemID)
 
-		f.Name:SetText(link and link:gsub("[%[%]]", ""))
+		local name = itemID and GetItemInfo(itemID)
+		local slotName = slotNames[id] and _G[slotNames[id]:upper()]
+
+		f.Name:SetText(link and link:gsub("[%[%]]", "") or slotName)
 		f.ID:SetText("(" .. id .. ")")
 
 		f.insert = SUG.inputType == "number" and id or name
 		f.insert2 = SUG.inputType ~= "number" and id or name
 
-		f.tooltipmethod = "SetHyperlink"
-		f.tooltiparg = link
+		if link then
+			f.tooltipmethod = "SetHyperlink"
+			f.tooltiparg = link
+		else
+			f.tooltiptitle = slotName
+			f.tooltiptext = TRANSMOGRIFY_INVALID_REASON1
+		end
 
 		f.Icon:SetTexture(GetItemIcon(itemID))
 	end
@@ -85,8 +121,10 @@ function Module:Table_GetSpecialSuggestions_2(suggestions, tbl, ...)
 	local atBeginning = SUG.atBeginning
 
 	for i = INVSLOT_FIRST_EQUIPPED, INVSLOT_LAST_EQUIPPED do
-		local itemID = GetInventoryItemID("player", i) -- get the itemID of the slot
-		self.Slots[i] = itemID and GetItemInfo(itemID)
+		if i ~= INVSLOT_RANGED then -- why did you not get rid of this, blizz?....
+			local itemID = GetInventoryItemID("player", i) -- get the itemID in the slot
+			self.Slots[i] = itemID and GetItemInfo(itemID) or _G[slotNames[i]:upper()]
+		end
 	end
 
 	if SUG.inputType == "number" then
@@ -101,7 +139,7 @@ function Module:Table_GetSpecialSuggestions_2(suggestions, tbl, ...)
 		end
 	else
 		for id, name in pairs(self.Slots) do
-			if strfindsug(strlower(name)) then
+			if name ~= "" and strfindsug(strlower(name)) then
 				suggestions[#suggestions + 1] = id
 			end
 		end
@@ -109,7 +147,7 @@ function Module:Table_GetSpecialSuggestions_2(suggestions, tbl, ...)
 end
 function Module:Entry_Colorize_1(f, id)
 	if id <= INVSLOT_LAST_EQUIPPED then
-		f.Background:SetVertexColor(.58, .51, .79, 1) -- color item slots warlock purple
+		f.Background:SetVertexColor(.23, .20, .29, 1) -- color item slots purpleish
 	end
 end
 function Module.Sorter_ByName(a, b)
