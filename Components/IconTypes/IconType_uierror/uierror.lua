e78fd2e528d9a64fe43eb4c835e024912a5bb000
cybeloras <ascott18@gmail.com>
2015-01-11 21:23:31 -0800

    Raised the sort order of icontype-specific notification triggers.

--- Components/IconTypes/IconType_uierror/uierror.lua
+++ Components/IconTypes/IconType_uierror/uierror.lua
@@ -65,7 +65,7 @@ Type:RegisterConfigPanel_XMLTemplate(165, "TellMeWhen_WhenChecks", {
 
 Type:RegisterConfigPanel_XMLTemplate(150, "TellMeWhen_UIErrorOptions")
 
-Type:RegisterIconEvent(61, "OnUIErrorEvent", {
+Type:RegisterIconEvent(5, "OnUIErrorEvent", {
 	category = L["ICONMENU_UIERROR"],
 	text = L["SOUND_EVENT_ONUIERROR"],
 	desc = L["SOUND_EVENT_ONUIERROR_DESC"],
