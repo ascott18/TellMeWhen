5c48e77ab8cb6eab34e086a8908a840fa61a5022
cybeloras <ascott18@gmail.com>
2016-07-17 22:12:43 -0700

    Minor changelog updates

--- TellMeWhen.lua
+++ TellMeWhen.lua
@@ -26,9 +26,9 @@ elseif strmatch(projectVersion, "%-%d+%-") then
 end
 
 TELLMEWHEN_VERSION_FULL = TELLMEWHEN_VERSION .. " " .. TELLMEWHEN_VERSION_MINOR
-TELLMEWHEN_VERSIONNUMBER = 80036 -- NEVER DECREASE THIS NUMBER (duh?).  IT IS ALSO ONLY INTERNAL (for versioning of)
+TELLMEWHEN_VERSIONNUMBER = 80037 -- NEVER DECREASE THIS NUMBER (duh?).  IT IS ALSO ONLY INTERNAL (for versioning of)
 
-TELLMEWHEN_FORCECHANGELOG = 80036 -- if the user hasn't seen the changelog until at least this version, show it to them.
+TELLMEWHEN_FORCECHANGELOG = 80037 -- if the user hasn't seen the changelog until at least this version, show it to them.
 
 if TELLMEWHEN_VERSIONNUMBER > 81000 or TELLMEWHEN_VERSIONNUMBER < 80000 then
 	-- safety check because i accidentally made the version number 414069 once
