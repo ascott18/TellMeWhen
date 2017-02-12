03a6400f938d474928779d10e92c3f8fdc07e0d8
cybeloras <ascott18@gmail.com>
2017-02-11 20:42:23 -0800

    Version bump again. Let's make this 8.3.0, shall we?

--- Options/CHANGELOG.lua
+++ Options/CHANGELOG.lua
@@ -3,7 +3,7 @@ if not TMW then return end
 TMW.CHANGELOG_LASTVER="6.2.6"
 
 TMW.CHANGELOG = [==[
-===v8.2.7===
+===v8.3.0===
 * New setting for Reactive Ability icons: Require activation border. 
 ** For all you prot warriors who like your Revenge procs.
 * New Condition: Spell Cost.
