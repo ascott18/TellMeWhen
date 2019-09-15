893c3fa4b87e502c5e6a6830292048d58d502873
Andrew Scott <ascott18@gmail.com>
2019-09-15 12:11:44 -0700

    Added an option to Spell Cooldown icons and Cooldown conditions to prevent the GCD from being ignored.

--- Options/CHANGELOG.lua
+++ Options/CHANGELOG.lua
@@ -7,6 +7,7 @@ TMW.CHANGELOG = [==[
 * The Missing Buffs/Debuffs icon type now sorts by lowest duration first.
 * Switched to DRList-1.0 (from DRData-1.0) for DR category data.
 * Added events to the Combat Event icon type for swing & spell dodges/blocks/parries.
+* Added an option to Spell Cooldown icons and Cooldown conditions to prevent the GCD from being ignored.
 
 ### Bug Fixes
 * Fixed an uncommon issue that could cause some event-driven icons to not update correctly after one of the units being tracked by an icon stops existing.
