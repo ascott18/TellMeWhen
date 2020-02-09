475a47a3843db31ccf0046bb192f63780a342aca
Andrew Scott <ascott18@gmail.com>
2020-02-08 22:24:08 -0800

    Fix #1764 - Fix resizing of the main configuration window, the color picker, and a few others.
    Don't use IsEnabled in Resizer_Generic because it won't be set when Resizer_Generic is used standlone.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 ## v8.7.4
 ### Bug Fixes
 * Fix #1762 - Suggestion list insertion via left-click not working due to bizarre new focus-clearing mechanism in WoW 8.3.
+* Fix #1764 - Fix resizing of the main configuration window, the color picker, and a few others.
 
 ## v8.7.3
 * Added a new "Any Totem" condition that will check all totem slots.
--- TellMeWhen.lua
+++ TellMeWhen.lua
@@ -44,7 +44,7 @@ elseif strmatch(projectVersion, "%-%d+%-") then
 end
 
 TELLMEWHEN_VERSION_FULL = TELLMEWHEN_VERSION .. " " .. TELLMEWHEN_VERSION_MINOR
-TELLMEWHEN_VERSIONNUMBER = 87401 -- NEVER DECREASE THIS NUMBER (duh?).  IT IS ALSO ONLY INTERNAL (for versioning of)
+TELLMEWHEN_VERSIONNUMBER = 87402 -- NEVER DECREASE THIS NUMBER (duh?).  IT IS ALSO ONLY INTERNAL (for versioning of)
 
 TELLMEWHEN_FORCECHANGELOG = 86005 -- if the user hasn't seen the changelog until at least this version, show it to them.
 
