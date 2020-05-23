bb4895cf586dea47efc43cd4b36104db1c5509f9
Andrew Scott <ascott18@gmail.com>
2020-05-23 09:52:59 -0700

    * Fix #1786 - TimerBar.lua:162: TexCoord out of range

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@
 ### Bug Fixes
 * Fix #1764 - Fix resizing of the main configuration window, the color picker, and a few others.
 * Fix #1790 - Attack Power condition doesn't work.
+* Fix #1786 - TimerBar.lua:162: TexCoord out of range
 
 ## v8.7.4
 ### Bug Fixes
--- Options/CHANGELOG.lua
+++ Options/CHANGELOG.lua
@@ -9,6 +9,7 @@ TMW.CHANGELOG = [==[
 ### Bug Fixes
 * Fix #1764 - Fix resizing of the main configuration window, the color picker, and a few others.
 * Fix #1790 - Attack Power condition doesn't work.
+* Fix #1786 - TimerBar.lua:162: TexCoord out of range
 
 ## v8.7.4
 ### Bug Fixes
