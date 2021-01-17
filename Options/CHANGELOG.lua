34e46ab6c74e644b7ffdc11549a976f648ee6afb
Andrew Scott <ascott18@gmail.com>
2021-01-16 17:18:59 -0800

    #1844 - The Totem icon type has been updated for Monks to better support the wide range of "totems" that Monks have.

--- Options/CHANGELOG.lua
+++ Options/CHANGELOG.lua
@@ -9,6 +9,7 @@ TMW.CHANGELOG = [==[
 * #1811 - The group/target point of a group will now be preserved when moving a shrunk group via click-and-drag.
 ### Bug Fixes
 * #1840 - Talents granted by Torghast powers are now correctly reflected by the Talent Learned condition.
+* #1844 - The Totem icon type has been updated for Monks to better support the wide range of "totems" that Monks have.
 
 ## v9.0.3
 ### Bug Fixes
