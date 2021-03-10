4acb65e57c4dff7afca137cbc6ec06659bb2c650
Andrew Scott <ascott18@gmail.com>
2021-03-09 17:54:46 -0800

    Fixed #1868 - Lua errors and broken sliders in WoW 9.0.5.

--- Options/CHANGELOG.lua
+++ Options/CHANGELOG.lua
@@ -8,6 +8,7 @@ TMW.CHANGELOG = [==[
 ### Bug Fixes
 * Fixed an error with the `[Name]` DogTag breaking when LibDogTag-Unit gets upgraded after TMW loads.
 * Fixed incorrect labels on Text Display editboxes after changing an icon's text layout.
+* Fixed #1868 - Lua errors and broken sliders in WoW 9.0.5.
 
 ## v9.0.4
 * New Condition: Torghast Anima Power Count
