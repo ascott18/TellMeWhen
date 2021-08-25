31a2f1e9270c801c921ac855bece65e3fbb4c672
Andrew Scott <ascott18@gmail.com>
2021-08-24 20:34:55 -0700

    Fixed #1914 - Meta icons that switch between shown/hidden OmniCC timer text without changing the duration of their timer now properly hide/show the timer text.

--- Options/CHANGELOG.lua
+++ Options/CHANGELOG.lua
@@ -7,6 +7,7 @@ TMW.CHANGELOG = [==[
 ### Bug Fixes
 * Fixed #1909 - IconConfig.lua:127: attempt to index field "CurrentTabGroup" (a nil value)
 * Fixed #1913 (LibDogTag-Stats-3.0/1) - Error with code "[SpellCrit]"
+* Fixed #1914 - Meta icons that switch between shown/hidden OmniCC timer text without changing the duration of their timer now properly hide/show the timer text.
 
 
 ## v9.1.0
