40a0e656b78a23b83ea9fe2d2fb1c789e760ac0d
Arieh Glazer <arieh@chegg.com>
2022-11-19 22:17:02 +0200

    added empowered stage

--- Components/Core/Conditions/Categories/Spells.lua
+++ Components/Core/Conditions/Categories/Spells.lua
@@ -38,6 +38,11 @@ Env.GetSpellCooldown = GetSpellCooldown
 Env.GetItemCooldown = GetItemCooldown
 
 
+Env.EmpoweredStage = 0
+Env.EmpoweredSpellName = ''
+Env.EmpoweredNumStages = 0
+
+
 local GetSpellCooldown = GetSpellCooldown
 function Env.CooldownDuration(spell, gcdAsUnusable)
 	if spell == "gcd" then
@@ -210,6 +215,89 @@ if TMW.isRetail then
 			local _, _, start, duration = GetSpellCharges(c.NameFirst)
 			local VALUE = duration and start + (duration - c.Level) or huge
 		]],
+	})
+
+	ConditionCategory:RegisterCondition(2.6,  "EMPOWEREDSTAGE", {
+	    text = L["EMPOWEREDSTAGE"],
+	    tooltip = L["EMPOWEREDSTAGE_DESC"],
+	    unit="player",
+		useSUG = "spell",	
+	    name = function(editbox)
+			editbox:SetTexts(L["SPELLTOCOMP1"], L["CNDT_ONLYFIRST"])
+		end,
+	    min = 0,
+	    max = 5,
+	    icon = "Interface\\Icons\\inv_10_enchanting2_elementalswirl_color1",
+	    tcoords = CNDT.COMMON.standardtcoords,
+
+	    specificOperators = {["<="] = true, [">="] = true, ["=="]=true, ["~="]=true},
+
+	    applyDefaults = function(conditionData, conditionSettings)
+	        local op = conditionSettings.Operator
+
+	        if not conditionData.specificOperators[op] then
+	            conditionSettings.Operator = "<="
+	        end
+	    end,
+
+		events = function(ConditionObject, c)
+			return
+				ConditionObject:GenerateNormalEventString("TMW_CNDT_EMPOWERED_UPDATED")
+		end,
+		funcstr = function(c)
+			local module = CNDT:GetModule("TMWST_EMPOWERED_CAST", true)
+			if not module then
+				module = CNDT:NewModule("TMWST_EMPOWERED_CAST", "AceEvent-3.0")
+				module.Timers = {}
+				module:RegisterEvent("UNIT_SPELLCAST_EMPOWER_START", function()
+					local unit = 'player'
+					local name, text, texture, startTime, endTime, isTradeSkill, notInterruptible, spellID, _, numStages = UnitChannelInfo(unit);
+
+					if not name then return end
+
+					Env.EmpoweredStage = 0
+					Env.EmpoweredSpellName = name
+					Env.EmpoweredNumStages = numStages + 1;
+
+					local sumDuration = 0;
+
+					TMW:Fire("TMW_CNDT_EMPOWERED_UPDATED")	
+
+					local getStageDuration = function(stage)
+						if stage == Env.EmpoweredNumStages then	
+							return GetUnitEmpowerHoldAtMaxTime(unit);
+						else
+							return GetUnitEmpowerStageDuration(unit, stage-1);
+						end
+					end;
+
+					for i = 1,Env.EmpoweredNumStages-1,1 do
+						local duration = getStageDuration(i);
+
+						if(duration > CASTBAR_STAGE_DURATION_INVALID) then
+							sumDuration = sumDuration + duration;
+							module.Timers[i] = C_Timer.NewTicker(sumDuration/1000, function()
+								Env.EmpoweredStage = i
+								TMW:Fire("TMW_CNDT_EMPOWERED_UPDATED")
+							end, 1)
+						end
+					end
+				end)
+
+				module:RegisterEvent("UNIT_SPELLCAST_EMPOWER_STOP", function()
+					for i=1,Env.EmpoweredNumStages do 
+						if module.Timers[i] then module.Timers[i]:Cancel() end
+					end		
+					module.Timers = {}
+					Env.EmpoweredStage = 0
+					Env.EmpoweredSpellName = ''
+					Env.EmpoweredNumStages = 0
+					TMW:Fire("TMW_CNDT_EMPOWERED_UPDATED")	
+				end)
+			end
+
+			return [[(strlower(c.NameFirst) == strlower(EmpoweredSpellName) and EmpoweredStage c.Operator c.Level)]]
+		end 
 	})
 end
 
